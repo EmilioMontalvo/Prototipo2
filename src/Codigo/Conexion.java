8910707d07bf0ac9fb384bfadfcb814ada24fa75
EmilioMontalvo <emilio0023@hotmail.com>
2022-02-27 19:43:14 -0500

    conexion

---
+++ src/Codigo/Conexion.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Codigo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+
+/**
+ *
+ * @author HP
+ */
+public class Conexion {
+    Connection con;
+    public Conexion(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","root","root");
+        } catch (Exception e) {
+            System.err.println("Error:" +e);
+        }
+    }
+    public static void main(String[] args) {
+        Conexion cn=new Conexion();
+        Statement st;
+        ResultSet rs;
+        try {
+            st=cn.con.createStatement();
+            rs=st.executeQuery("SHOW tables");
+            while (rs.next()) {                
+                System.out.println(rs.getString("Tables_in_mydb"));
+            }
+            cn.con.close();
+        } catch (Exception e) {
+        }
+        
+    }
+}
+
